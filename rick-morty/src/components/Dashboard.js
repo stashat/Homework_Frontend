ac780ad6e7fac7c13064e0688a50af16b33a9d9f
stashat2 <stasa.tadic1@gmail.com>
2020-06-15 17:41:54 +0200

    added chart

--- rick-morty/src/components/Dashboard.js
+++ rick-morty/src/components/Dashboard.js
@@ -1,34 +1,64 @@
 import React from 'react';
 import axios from 'axios';
+import { HorizontalBar } from 'react-chartjs-2'
 
 
 class Dashboard extends React.Component {
-    constructor() {
-        super(props)
+    constructor(props) {
+        super(props);
         this.state = {
-
+            countries: []
         }
-    }
-    state = {
-        countries: []
     }
 
     componentDidMount() {
         axios.get('https://api.covid19api.com/summary').then(res => {
             console.log(res)
             this.setState({ countries: res.data.Countries })
-
-            var countries_array = this.state.countries.map(info => info.Country)
-            console.log(countries_array)
         })
     }
     render() {
-        return (
-            <ul>
-                {this.state.countries.map(info => <li>{info.Country}</li>)}
-            </ul>
-        );
+        return (<div>
+            <HorizontalBar
+                height={5000}
+                data={{
+                    labels: this.state.countries.map(info => info.Country),
+                    datasets: [
+                        {
+                            label: 'New Deaths',
+                            data: this.state.countries.map(info => info.NewDeaths),
+                            backgroundColor: "rgba(240, 52, 52, 1)"
+                        },
+
+                        {
+                            label: 'Total Deaths',
+                            data: this.state.countries.map(info => info.TotalDeaths),
+                            backgroundColor: "rgba(30, 139, 195, 1)"
+
+                        }
+
+                    ]
+
+                }}
+                options={{
+                    maintainAspectRatio: false,
+                    title: {
+                        display: true,
+                        text: 'COVID 19 Stats',
+                        fontSize: 25
+                    },
+                    legend: {
+                        display: true,
+                        position: 'top',
+
+                    }
+                }}
+            />
+        </div>);
     }
 }
+
+
+
 
 export default Dashboard;
